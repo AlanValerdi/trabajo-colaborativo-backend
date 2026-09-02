a744cdaf0d81b401732e87179de44489bfa10271
AlanValerdi <alanvalerdi@AlanValerdis-MacBook-Air.local>
2026-09-01 20:08:32 -0600

    feat: setup inicial

--- app/main.py
+++ app/main.py
@@ -1,1 +1,4 @@
 from fastapi import FastAPI
+from app.db.database import Base
+
+app = FastAPI(title="servicio-trabajo-colaborativo")
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,9 @@
 from sqlalchemy import String
-from app.db
+from db.database import Base
+from sqlmodel import SQLModel, Field
 
-class User(Base):
+class User(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    
+    
+
