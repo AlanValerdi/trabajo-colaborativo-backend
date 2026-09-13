ca1a0cc5af71f93ed9705807d03cfd17e3c1e736
Jorge <susurito5@gmail.com>
2026-09-13 14:10:14 -0600

    Preparacion del back para el front

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.urls import register_routers
 
 app = FastAPI(title="servicio-trabajo-colaborativo")
+
+# Configuración CORS
+origins = [
+    "http://localhost:4200",  # Angular por defecto
+    "http://127.0.0.1:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 register_routers(app)
 
